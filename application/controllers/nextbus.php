435066f31bdc85114052a90f7994f5a4e7a04292
Steven Lu <tacticalazn@gmail.com>
2012-04-07 13:46:04 -0400

    changing stuff

--- application/controllers/nextbus.php
+++ application/controllers/nextbus.php
@@ -2,7 +2,7 @@
 
 class Nextbus extends CI_Controller {
 
-   public function get_route_config($agency)
+   public function get_config($agency)
    {
       $this->load->model('nextbus_model');
       echo json_encode($this->nextbus_model->get_config($agency)); 
