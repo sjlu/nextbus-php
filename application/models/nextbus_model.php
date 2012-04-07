1620576a9e662f6fb6d28f846a597e5194d5e2e8
Steven Lu <tacticalazn@gmail.com>
2012-04-07 18:14:24 -0400

    using file cache

--- application/models/nextbus_model.php
+++ application/models/nextbus_model.php
@@ -31,7 +31,7 @@ class Nextbus_model extends CI_Model {
       $this->add_param('a', $agency);
       $this->add_param('command', 'routeConfig');
          
-      $this->load->driver('cache', array('adapter' => 'memcached', 'backup' => 'file'));
+      $this->load->driver('cache', array('adapter' => 'file'));
       
       if (!$data = $this->cache->get($this->PARAMS))
       {
