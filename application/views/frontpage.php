be44acf05bdb852e173b684a627f99851c466194
Steven Lu <tacticalazn@gmail.com>
2012-07-19 14:59:32 -0400

    Adding runtime.

--- application/views/frontpage.php
+++ application/views/frontpage.php
@@ -1,4 +1,6 @@
 <div class="container">
+   <h2>Runtime</h2>
+   <pre>{elapsed_time}s</pre>
    <h2>Route Configuration</h2>
    <p><?= base_url('nextbus/get_config/rutgers') ?></p>
    <pre><?= json_encode($config); ?></pre>
